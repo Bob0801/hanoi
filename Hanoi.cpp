c795eb8c96458d6a9dc5d4b80cc237e07366355b
Bob0801 <92445208+Bob0801@users.noreply.github.com>
2024-05-27 22:28:30 +0800

    Add files via upload

---
+++ Hanoi.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int step = 0;
+
+void tower(int n, char a, char b, char c) {
+    if (n == 1) {
+        cout << a << "-->" << c << endl;
+        step++;
+    } else {
+        tower(n - 1, a, c, b);
+        cout << a << "-->" << c << endl;
+        step++;
+        tower(n - 1, b, a, c);
+    }
+}
+
+int main() {
+    int n;
+    cout << "請輸入層數:";
+    cin >> n;
+    tower(n, 'A', 'B', 'C');
+    cout << "共" << step << "步" << endl;
+}
